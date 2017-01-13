445ccfa49d033a0f8b83d424e92347248cb1bc52
Tilman Baumann <tilman.baumann@canonical.com>
2017-01-13 16:48:34 +0100

    Initial naiive implementation

--- README.ex
+++ README.ex
@@ -1,65 +1,33 @@
 # Overview
 
-Describe the intended usage of this charm and anything unique about how this
-charm relates to others here.
+Blocks SSH and allows access only from other nodes running the same charm and
+from a configurable whitelist of networks and hosts.
 
-This README will be displayed in the Charm Store, it should be either Markdown
-or RST. Ideal READMEs include instructions on how to use the charm, expected
-usage, and charm features that your audience might be interested in. For an
-example of a well written README check out Hadoop:
-http://jujucharms.com/charms/precise/hadoop
-
-Use this as a Markdown reference if you need help with the formatting of this
-README: http://askubuntu.com/editing-help
-
-This charm provides [service][]. Add a description here of what the service
-itself actually does.
-
-Also remember to check the [icon guidelines][] so that your charm looks good
-in the Juju GUI.
+This charm covers a very special use case which came in a customer environment.
+In this case ssh access was only permitted from other openstack compute nodes,
+so this charm is delpoyed on all compute nodes.
+It may only be useful as a template to implement similar things.
 
 # Usage
 
 Step by step instructions on using the charm:
 
-juju deploy servicename
+juju deploy iptables-peer-ssh --to unit
 
-and so on. If you're providing a web service or something that the end user
-needs to go to, tell them here, especially if you're deploying a service that
-might listen to a non-default port.
-
-You can then browse to http://ip-address to configure the service.
+This charm is designed to run together with other charm on the same unit.
+Running it stand alone makes very little sense.
 
 ## Scale out Usage
 
-If the charm has any recommendations for running at scale, outline them in
-examples here. For example if you have a memcached relation that improves
-performance, mention it here.
+Add units to all hosts which should be covered by this ssh permisson.
 
-## Known Limitations and Issues
-
-This not only helps users but gives people a place to start if they want to help
-you add features to your charm.
 
 # Configuration
 
-The configuration options will be listed on the charm store, however If you're
-making assumptions or opinionated decisions in the charm (like setting a default
-administrator password), you should detail that here so the user knows how to
-change it immediately, etc.
-
-# Contact Information
-
-Though this will be listed in the charm store itself don't assume a user will
-know that, so include that information here:
-
-## Upstream Project Name
-
-  - Upstream website
-  - Upstream bug tracker
-  - Upstream mailing list or contact information
-  - Feel free to add things if it's useful for users
+Make sure you allow access from your control nodes if you need ssh access for
+debug purposes.
 
 
-[service]: http://example.com
-[icon guidelines]: https://jujucharms.com/docs/stable/authors-charm-icon
+## iptables-peer-ssh
+  - https://github.com/tbaumann/iptables-peer-ssh
+  - https://github.com/tbaumann/iptables-peer-ssh/issues
--- reactive/iptables_peer_ssh.py
+++ reactive/iptables_peer_ssh.py
@@ -1,17 +1,116 @@
-from charms.reactive import when, when_not, set_state
+from charms.reactive import (
+    when,
+    when_not,
+    set_state,
+    remove_state,
+    RelationBase,
+    hook
+)
+from charmhelpers.core import hookenv
+from charmhelpers.core.hookenv import (
+    related_units,
+    local_unit,
+    relation_ids,
+    relation_get,
+    log,
+    status_set,
+    config,
+    in_relation_hook
+)
+from charms.reactive.helpers import data_changed
+from subprocess import call
 
 
 @when_not('iptables-peer-ssh.installed')
 def install_iptables_peer_ssh():
-    # Do your setup here.
-    #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
-    #
     set_state('iptables-peer-ssh.installed')
+
+
+@when('iptables-peer-ssh.installed')
+@when_not('iptables.started')
+def setup_iptables():
+    status_set('maintenance', 'Setting up IPTables')
+    ipset_create('ssh-peers', 'hash:ip')
+    ipset_create('ssh-allow-hosts', 'hash:ip')
+    ipset_create('ssh-allow-networks', 'hash:net')
+    call('iptables -A INPUT -p tcp --dport ssh -m set --match-set ssh-peers src -j ACCEPT', shell=True)
+    call('iptables -A INPUT -p tcp --dport ssh -m set --match-set ssh-allow-hosts src -j ACCEPT', shell=True)
+    call('iptables -A INPUT -p tcp --dport ssh -m set --match-set ssh-allow-networks src -j ACCEPT', shell=True)
+
+    # call('iptables -A INPUT -p tcp --dport ssh -j ACCEPT', shell=True)  # Drop the rest
+    status_set('active', 'Ready')
+    set_state('iptables.started')
+
+
+@hook('stop')
+def stop_iptables():
+    status_set('maintenance', 'Stopping IPTables')
+    call('iptables -D INPUT -p tcp --dport ssh -m set --match-set ssh-peers src -j ACCEPT', shell=True)
+    call('iptables -D INPUT -p tcp --dport ssh -m set --match-set ssh-allow-hosts src -j ACCEPT', shell=True)
+    call('iptables -D INPUT -p tcp --dport ssh -m set --match-set ssh-allow-networks src -j ACCEPT', shell=True)
+    call('iptables -D INPUT -p tcp --dport ssh -j ACCEPT', shell=True)  # Drop the rest
+    ipset_destroy('ssh-peers')
+    ipset_destroy('ssh-allow-hosts')
+    ipset_destroy('ssh-allow-networks')
+    remove_state('iptables.started')
+    status_set('maintenance', 'Stopped')
+
+
+@hook('upgrade-charm')
+def upgrade_charm():
+    stop_iptables()
+    setup_iptables()
+
+
+@when('ssh-peers.joined ')
+def connected(peers):
+    hosts = peers.units()
+    if data_changed('ssh-peers', hosts):
+        ipset_update('ssh-peers', hosts)
+    remove_state('ssh-peers.joined')
+
+
+@when('ssh-peers.departed')
+def departed(peers):
+    hosts = peers.units()
+    if data_changed('ssh-peers', hosts):
+        ipset_update('ssh-peers', hosts)
+    remove_state('ssh-peers.departed')
+
+
+@when('config.changed.ssh-allow-hosts')
+def ssh_allow_hosts_changed():
+    config = hookenv.config()
+    hosts = config['ssh-allow-hosts'].split()
+    if data_changed('ssh-allow-hosts', hosts):
+        ipset_update('ssh-allow-hosts', hosts)
+
+
+@when('config.changed.ssh-allow-networks')
+def ssh_allow_networks_changed():
+    config = hookenv.config()
+    hosts = config['ssh-allow-networks'].split()
+    if data_changed('ssh-allow-networks', hosts):
+        ipset_update('ssh-allow-networks', hosts)
+
+
+def ipset_create(name, type):
+    call(['ipset', 'create',  name, type])
+    call(['ipset', 'create',  name + '-tmp', type])
+
+
+def ipset_destroy(name):
+    call(['ipset', 'destroy',  name])
+    call(['ipset', 'destroy',  name + '-tmp'])
+
+
+def ipset_update(name, hosts):
+    log("Updating {} ipset".format(name))
+    tmpname = name + '-tmp'
+    call(['ipset', 'flush', tmpname])
+    for host in hosts:
+        log("Adding {} to ipset {}".format(host, tmpname))
+        call(['ipset', 'add', tmpname, host])
+    call(['ipset', 'swap', tmpname, name])
+    call(['ipset', 'flush', tmpname])
+    log("swapped ipsed {}".format(name))
